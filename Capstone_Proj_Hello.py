257c89414f8665719fb264ef430ab72310ba0e23
Fahad Saud Al Ghatrifi <fahadssg@Fahads-MacBook-Pro.local>
2020-05-27 03:37:53 +0400

    adds a python script that prints a message greeting Github

---
+++ Capstone_Proj_Hello.py
@@ -0,0 +1,7 @@
+<h1><center>This notebook will be mainly used for the Capstone Project</center></h1>
+<h1><center>Capstone Project</center></h1>
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
